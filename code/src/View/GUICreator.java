a1e0bbf83fa5d5214fa762aab1c53a3e15400f98
AntonMelekhavets550504 <antohage@gmail.com>
2017-11-24 18:55:59 +0300

    Layouts for GUI were created

---
+++ code/src/View/GUICreator.java
@@ -0,0 +1,18 @@
+package View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUICreator extends Application {
+	private Stage baseStage;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage baseStage) throws Exception {
+	}
+
+}
